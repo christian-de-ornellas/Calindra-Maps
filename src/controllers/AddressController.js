ff57206fe66c0e74d1ae54cfb08e805ed673caf8
Christian Possidonio <ch_ris@hotmail.com>
2019-08-15 23:16:58 -0300

    Adicionei o endereço completo para exibir junto.

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -4,12 +4,12 @@ const axios = require('axios')
 
 module.exports = {
     async findAddress(req, res) {
-       
+
         //Config de acesso da api.s
         ConfApi = {
             url: 'https://maps.googleapis.com/maps/api/geocode/json?address=',
-           // address: 'Av. Rio branco 1 - Centro, Rio de Janeiro - RJ, Rua 19 de fevereiro, 34 - Botafogo, Rio de Janeiro - RJ, Rua laranjeiras 27, Vilar dos teles, Rua Uruguaiana 22, Centro',
-           address: req.params.end,
+            // address: 'Av. Rio branco 1 - Centro, Rio de Janeiro - RJ, Rua 19 de fevereiro, 34 - Botafogo, Rio de Janeiro - RJ, Rua laranjeiras 27, Vilar dos teles, Rua Uruguaiana 22, Centro',
+            address: req.params.end,
             keyGoogle: 'AIzaSyChxyljBxrQ9yvdS5BOJEWywv1C45lpprw'
         }
 
@@ -20,26 +20,20 @@ module.exports = {
         const points = geoApi.map(function (point) {
 
             const local = {
+                address: point.formatted_address,
                 lat: point.geometry.location.lat,
-                lng: point.geometry.location.lng,
+                lng: point.geometry.location.lng
             }
-
 
             return local
         })
 
         const distancePoints = {
             cordenades: points,
-            distance: Math.sqrt((points[0].lat - points[0].lng) * (points[1].lat - points[1].lng))
+            distance: Math.sqrt((points[0].lat - points[0].lng) * (points[1].lat - points[1].lng)),
 
         }
 
-        //RECEBER DOIS OU MAIS ENDERECOS E CALCULAR A DISTANCIA
-        //const euclidean = (p, q) => Math.sqrt( (p[0]-q[0]) * (p[1]-q[1]));
-
-        // CALCULODEDISTANCIA EUCLIDIANA 
-
-        // RETORNARAS DISTANCIAS CALCULADAS ENTRE TODOS OS ENDERECOS E INDICAR OS ENDECOS MAIS PROXIMOS
 
         return res.send(distancePoints)
     }
