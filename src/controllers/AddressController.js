882af1a152bcb3576f25c2f755b29613515fd816
Christian Possidonio <ch_ris@hotmail.com>
2019-08-22 19:34:26 -0300

    Calculo euclidiano finalizado

--- src/controllers/AddressController.js
+++ src/controllers/AddressController.js
@@ -9,7 +9,7 @@ module.exports = {
             url: 'https://maps.googleapis.com/maps/api/geocode/json?address=',
             // address: 'Av. Rio branco 1 - Centro, Rio de Janeiro - RJ, Rua 19 de fevereiro, 34 - Botafogo, Rio de Janeiro - RJ, Rua laranjeiras 27, Vilar dos teles, Rua Uruguaiana 22, Centro',
             address: req.params.end,
-            keyGoogle: 'YOUR-KEY'
+            keyGoogle: 'You-key'  // Adicione a sua chave.
         }
 
         //Busca na Api do Google a geolocalização dos endereços citados.
@@ -22,6 +22,7 @@ module.exports = {
         const coordinantes = api.map((coordinante) => {
 
             const data = {
+                address: coordinante.formatted_address,
                 lat: coordinante.geometry.location.lat,
                 lng: coordinante.geometry.location.lng
             }
